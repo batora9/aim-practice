9aeaac4fdd087b25019434f60244158665d5cd5f
syun_m <sm.net_minecraft.hacker@outlook.jp>
2023-07-25 00:12:46 +0900

    version 1.0

---
+++ aim practice/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace aim_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel10 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel11 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel7 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel8 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel9 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel10,
+            this.toolStripStatusLabel11,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel6,
+            this.toolStripStatusLabel7,
+            this.toolStripStatusLabel8,
+            this.toolStripStatusLabel9});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(987, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(61, 17);
+            this.toolStripStatusLabel1.Text = "設定バー　";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(149, 17);
+            this.toolStripStatusLabel3.Text = "ボタンの大きさ:　　　　　　";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(165, 17);
+            this.toolStripStatusLabel2.Text = "ボタンを押す回数:　　　　　　";
+            // 
+            // toolStripStatusLabel10
+            // 
+            this.toolStripStatusLabel10.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel10.Name = "toolStripStatusLabel10";
+            this.toolStripStatusLabel10.Size = new System.Drawing.Size(54, 17);
+            this.toolStripStatusLabel10.Text = "残り回数:";
+            // 
+            // toolStripStatusLabel11
+            // 
+            this.toolStripStatusLabel11.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel11.Name = "toolStripStatusLabel11";
+            this.toolStripStatusLabel11.Size = new System.Drawing.Size(19, 17);
+            this.toolStripStatusLabel11.Text = "20";
+            this.toolStripStatusLabel11.Click += new System.EventHandler(this.toolStripStatusLabel11_Click);
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(68, 17);
+            this.toolStripStatusLabel4.Text = "総クリック数:";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel5.Text = "0";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(56, 17);
+            this.toolStripStatusLabel6.Text = "クリック数:";
+            // 
+            // toolStripStatusLabel7
+            // 
+            this.toolStripStatusLabel7.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel7.Name = "toolStripStatusLabel7";
+            this.toolStripStatusLabel7.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel7.Text = "0";
+            // 
+            // toolStripStatusLabel8
+            // 
+            this.toolStripStatusLabel8.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel8.Name = "toolStripStatusLabel8";
+            this.toolStripStatusLabel8.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel8.Text = "命中率:";
+            // 
+            // toolStripStatusLabel9
+            // 
+            this.toolStripStatusLabel9.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel9.Name = "toolStripStatusLabel9";
+            this.toolStripStatusLabel9.Size = new System.Drawing.Size(23, 17);
+            this.toolStripStatusLabel9.Text = "0%";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 180);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "--";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(305, 0);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(59, 20);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(933, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "リセット";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(987, 602);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(750, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aim practice v2.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel7;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel8;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel9;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel10;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel11;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ aim practice/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aim_practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int click;
+        private int total;
+
+        private int s;
+        private int f;
+        //form size
+        private int w;
+        private int h;
+
+        private int count;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("10%");
+            comboBox1.Items.Add("30%");
+            comboBox1.Items.Add("50%");
+            comboBox1.Items.Add("75%");
+            comboBox1.Items.Add("100%");
+            comboBox1.SelectedIndex = 3;
+
+            comboBox2.Items.Add("1");
+            comboBox2.Items.Add("5");
+            comboBox2.Items.Add("10");
+            comboBox2.Items.Add("20");
+            comboBox2.Items.Add("30");
+            comboBox2.Items.Add("50");
+            comboBox2.Items.Add("70");
+            comboBox2.Items.Add("100");
+
+            comboBox2.SelectedIndex = 3;
+            //初期設定
+            w = 675;
+            h = 340;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            s = comboBox1.SelectedIndex;
+            switch (s)
+            {
+                case 0:
+                    this.button1.Size = new Size(25, 23);
+                    break;
+                case 1:
+                    this.button1.Size = new Size(50, 45);
+                    break;
+                case 2:
+                    this.button1.Size = new Size(125, 90);
+                    break;
+                case 3:
+                    this.button1.Size = new Size(200, 180);
+                    break;
+                case 4:
+                    this.button1.Size = new Size(300, 250);
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            f = comboBox2.SelectedIndex;
+            switch (f)
+            {
+                case 0:
+                    click = 1;
+                    break;
+                case 1:
+                    click = 5;
+                    break;
+                case 2:
+                    click = 10;
+                    break;
+                case 3:
+                    click = 20;
+                    break;
+                case 4:
+                    click = 30;
+                    break;
+                case 5:
+                    click = 50;
+                    break;
+                case 6:
+                    click = 70;
+                    break;
+                case 7:
+                    click = 100;
+                    break;
+            }
+            button1.Text = click.ToString();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //System.Drawing.Size tSize = this.ClientSize;
+            switch (s)
+            {
+                case 0:
+                    //this.button1.Size = new Size(25, 23);
+                    w = ClientSize.Width - 53;
+                    h = ClientSize.Height - 74;
+                    break;
+                case 1:
+                    //this.button1.Size = new Size(50, 45);
+                    w = ClientSize.Width - 78;
+                    h = ClientSize.Height - 96;
+                    break;
+                case 2:
+                    //this.button1.Size = new Size(125, 90);
+                    w = ClientSize.Width - 153;
+                    h = ClientSize.Height - 141;
+                    break;
+                case 3:
+                    //this.button1.Size = new Size(200, 180);
+                    w = ClientSize.Width - 228;
+                    h = ClientSize.Height - 231;
+                    break;
+                case 4:
+                    //this.button1.Size = new Size(300, 250);
+                    w = ClientSize.Width - 328;
+                    h = ClientSize.Height - 301;
+                    break;
+            }
+            button2.Location = new Point(ClientSize.Width - 54, 1);
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            total++;
+            toolStripStatusLabel5.Text = total.ToString();
+            if (count != 0 && total != 0)
+            {
+                double percent = Math.Floor(((double)count / total) * 100);
+                toolStripStatusLabel9.Text = percent.ToString() + "%";
+            }
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            total++;
+            count++;
+            toolStripStatusLabel5.Text = total.ToString();
+            toolStripStatusLabel7.Text = count.ToString();
+
+            click--;
+            button1.Text = click.ToString();
+            if (click == 0)
+            {
+                System.Random r = new System.Random();
+                int left = r.Next(12, w);
+                int top = r.Next(25, h);
+
+                this.button1.Location = new Point(left, top);
+                int f = comboBox2.SelectedIndex;
+                switch (f)
+                {
+                    case 0:
+                        click = 1;
+                        break;
+                    case 1:
+                        click = 5;
+                        break;
+                    case 2:
+                        click = 10;
+                        break;
+                    case 3:
+                        click = 20;
+                        break;
+                    case 4:
+                        click = 30;
+                        break;
+                    case 5:
+                        click = 50;
+                        break;
+                    case 6:
+                        click = 70;
+                        break;
+                    case 7:
+                        click = 100;
+                        break;
+                }
+                button1.Text = click.ToString();
+            }
+            if (count != 0 && total != 0)
+            {
+                double percent = Math.Floor(((double)count / total) * 100);
+                toolStripStatusLabel9.Text = percent.ToString() + "%";
+            }
+            toolStripStatusLabel11.Text = click.ToString();
+        }
+
+        private void toolStripStatusLabel11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(
+        "記録をリセットしますか？",
+        "Aim practice",
+        MessageBoxButtons.YesNo,  // ボタンの一覧は MessageBoxButtons 参照
+        MessageBoxIcon.Question   // アイコンの一覧は  MessageBoxIcon 参照
+    )
+    == DialogResult.Yes)
+            {
+                total = 0;
+                count = 0;
+                toolStripStatusLabel5.Text = total.ToString();
+                toolStripStatusLabel7.Text = count.ToString();
+                toolStripStatusLabel9.Text = "0%";
+            }
+        }
+    }
+}
